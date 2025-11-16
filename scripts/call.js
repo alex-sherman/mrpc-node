bb1bfa3d907eec5e1f35f5e5835097ecf94472fd
Alex Sherman <asherman1024@gmail.com>
2025-11-16 17:33:26 -0600

    Implement matter bridge.

--- scripts/call.js
+++ scripts/call.js
@@ -1,4 +1,6 @@
 import MRPC from "../src/mrpc.js"
 
-console.log(await (new MRPC()).Call(process.argv.slice(2)[0], JSON.parse(process.argv.slice(2)[1])))
+let value = process.argv.slice(2)[1];
+value = value && JSON.parse(value)
+console.log(await (new MRPC()).Call(process.argv.slice(2)[0], value))
 process.exit(0);
--- src/mrpc.js
+++ src/mrpc.js
@@ -6,19 +6,20 @@ export default class MRPC {
     this.id = crypto.randomUUID();
     this.server = createSocket("udp4");
     this.server.on("message", this.OnRecv.bind(this));
-    this.server.bind(50123);
+    this.server.bind({ port: 50123, reuseAddr: true });
     this.rpcId = 0;
     this.pending = {};
+  }
+
+  close() {
+    this.server.close();
   }
 
   OnRecv(msg, rinfo) {
     try {
       const obj = JSON.parse(msg);
       if (obj.dst !== this.id) return;
-      if (!(obj.id in this.pending)) { 
-        console.log(obj)
-        return;
-      }
+      if (!(obj.id in this.pending)) return;
       this.pending[obj.id].resolve(obj.result);
       delete this.pending[obj.id];
     } catch (error) {
@@ -27,23 +28,24 @@ export default class MRPC {
   }
 
   Call(dst, value) {
-    const rpcId = this.rpcId += 1;
+    const rpcId = (this.rpcId += 1);
     let payload = JSON.stringify({ src: this.id, id: rpcId, dst: dst, value: value });
     let send = () => this.server.send(payload, 50123, "192.168.1.255");
     send();
     const promise = new Promise((resolve, reject) => {
       let retries = 0;
-      let scheduleRetry = () => setTimeout(() => {
-        if (!(rpcId in this.pending)) return;
-        retries += 1;
-        if (retries < 10) {
-          send();
-          scheduleRetry();
-          return;
-        }
-        delete this.pending[rpcId];
-        reject("retries exceeded");
-      }, 100);
+      let scheduleRetry = () =>
+        setTimeout(() => {
+          if (!(rpcId in this.pending)) return;
+          retries += 1;
+          if (retries < 10) {
+            send();
+            scheduleRetry();
+            return;
+          }
+          delete this.pending[rpcId];
+          reject("retries exceeded");
+        }, 100);
       scheduleRetry();
       this.pending[rpcId] = { resolve };
     });
